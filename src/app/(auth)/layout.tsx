f79bdce0cac9d84a74ab5fb2d13a11fa96fd5ede
Azalmonawwar <monawwarazal@gmail.com>
2024-01-31 20:34:04 +0530

    fixes in auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider >
       <html lang="en">
         <body className={inter.className}>
           <main className="h-screen flex items-center justify-center"> 
